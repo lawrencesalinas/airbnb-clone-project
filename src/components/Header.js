4022cd487cbd83a953ab0f0e4d10fb74fc57a3ef
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-18 08:39:21 -0500

    created header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+              <img
+                    className="airbnb_icon"
+                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    alt=""
+                />
+                <div className='header_center'>
+                    <input type = 'text'/>
+                    <SearchIcon />
+                </div>
+        </div>
+    )
+}
+
+export default Header
