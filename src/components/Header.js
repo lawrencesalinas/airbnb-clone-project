5cb939af851274f3cfdd0b6b3c040626c8b76204
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-18 08:53:38 -0500

    added all the icons for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Avatar } from '@mui/material';
 import './Header.css'
+
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
               <img
                     className="airbnb_icon"
                     src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
@@ -12,6 +17,15 @@ function Header() {
                 <div className='header_center'>
                     <input type = 'text'/>
                     <SearchIcon />
+                </div>
+
+                <div className = 'header_right'>
+                    <p>Become a Host</p>
+                    <LanguageIcon/>
+                    <KeyboardArrowDownIcon/>
+                    <Avatar/>
+
+
                 </div>
         </div>
     )
