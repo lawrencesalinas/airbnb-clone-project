8345c619becc542c307459fe9baee7703c82949d
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-18 13:46:25 -0500

    added calendar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './Banner.css'
 import {Button} from '@mui/material'
 import { useState } from 'react'
+import Search from './Search'
 
 function Banner() {
     const[showSearch,setShowSearch]=useState(false)
     return (
         <div className = 'banner'>
             <div className='banner_search'>
-                {showSearch && <h1>SHOW DATE PICKER</h1>}
+                {showSearch && <Search/>}
                 <Button onClick={() => setShowSearch(!showSearch)} className = 'banner_searchButton' variant='outlined' >Search Dates</Button>
             </div>
             <div className='banner_info'>
