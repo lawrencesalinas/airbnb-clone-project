8490d3f3d980ffae6fb298634d6fb08253b6a570
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-19 05:51:02 -0500

    search page complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,18 @@ import LanguageIcon from '@mui/icons-material/Language';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Avatar } from '@mui/material';
 import './Header.css'
-
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div className='header'>
+            <Link to ='/'>
               <img
                     className="airbnb_icon"
                     src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
                     alt=""
                 />
+                </Link>
                 <div className='header_center'>
                     <input type = 'text'/>
                     <SearchIcon />
