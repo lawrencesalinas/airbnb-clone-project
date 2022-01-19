0168aa73546e66ef3950f3bbc690c3dd319439ea
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-19 02:49:55 -0500

    footer is done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './screens/Home';
 import Header from './components/Header'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header/>
       <Home/>
+      <Footer/>
+
     {/* Header
       Banner
       Search
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className='footer'>
             <p>@ 2020 Airbnb clone! no rights reserved - this is a demo!</p>
-            <p>Privacy Terms Sitemap Company Details</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
         
         </div>
     )
