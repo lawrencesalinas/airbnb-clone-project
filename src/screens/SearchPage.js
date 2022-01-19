8e9f6ede2194e9335d947919ae9198f0c4ce01a2
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-19 04:15:02 -0500

    creating search result component

--- src/screens/SearchPage.js
+++ src/screens/SearchPage.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './SearchPage.css'
+import { Button } from '@mui/material'
 
 function SearchPage() {
     return (
         <div className='SearchPage'>
            <div className='searchPage_info'>
-               
+           <p>62 stays · 26 august to 30 august · 2 guest</p>
+           <h1>Stays nearby</h1>
+           <Button variant="outlined">Cancellation Flexibility</Button>
+                <Button variant="outlined">Type of place</Button>
+                <Button variant="outlined">Price</Button>
+                <Button variant="outlined">Rooms and beds</Button>
+                <Button variant="outlined">More filters</Button>
                </div>
             
         </div>
