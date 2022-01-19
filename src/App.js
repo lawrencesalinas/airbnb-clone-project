79c506e71fb1bcfa553703fbaaac8e20da03d671
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-19 03:15:49 -0500

    creating the search page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './screens/Home';
-import Header from './components/Header'
-import Footer from './components/Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./screens/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+import SearchPage from "./screens/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <BrowserRouter>
+        <Header />
 
-    {/* Header
-      Banner
-      Search
-      Cards
-    BAnner */}
+        <Routes>
+         
+           <Route path='/search'element={<SearchPage/>}/>
+           <Route path='/' element={<Home/>}/>
+       
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/screens/SearchPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SearchPage.css'
+
+function SearchPage() {
+    return (
+        <div className='SearchPage'>
+           <div className='searchPage_info'>
+               
+               </div>
+            
+        </div>
+    )
+}
+
+export default SearchPage
