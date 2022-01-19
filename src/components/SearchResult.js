7f918806cac87a407ae629e766de1ea12846e299
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-19 05:46:15 -0500

    finish searchresult component

---
+++ src/components/SearchResult.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./SearchResult.css";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import StarIcon from "@mui/icons-material/Star";
+
+function SearchResult({
+  img,
+  location,
+  title,
+  description,
+  star,
+  price,
+  total,
+}) {
+  return (
+    <div className="searchResult">
+      <img src={img} alt="" />
+      <FavoriteBorderIcon className="searchResult_heart" />
+      <div className="searchResult_info">
+
+      <div className="searchResult_infoTop">
+        <h2>{location}</h2>
+        <h3>{title}</h3>
+        <p>_____</p>
+        <p>{description}</p>
+      </div>
+
+      <div className="searchResult_infoBottom">
+        <div className="searchResult_stars">
+          <StarIcon className="searchResult_star" />
+          <p>
+            <strong>{star}</strong>
+          </p>
+        </div>
+
+        <div className="searchResults_price">
+          <h2>{price}</h2>
+          <p>{total}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default SearchResult;
